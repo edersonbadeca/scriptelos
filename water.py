1bec89730c7a91dbc8cdc07c1a348ad23d74e5db
Ederson Badeca <edersonbadeca@gmail.com>
2021-04-09 21:49:35 -0300

    =0)

---
+++ water.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+#################################################################
+# Macosx only.                                                  #
+# Changes message, title, timing, source, sound just be happy.  #
+#################################################################
+import subprocess
+import time
+
+command = '''
+on run argv
+  display notification (item 2 of argv) with title (item 1 of argv)
+end run
+'''
+
+
+def notify():
+    _title = "Hora de ficar mais saudável"
+    _message = "Levante e beba um copo de aguá."
+    while True:
+        call_system(['afplay', '/System/Library/Sounds/Glass.aiff'])
+        call_system(['osascript', '-e', command, _title, _message])
+        time.sleep(300)
+
+
+def call_system(cmd):
+    subprocess.call(cmd)
+
+
+if __name__ == '__main__':
+    notify()
